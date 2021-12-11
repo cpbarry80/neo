7e7fcbe1d58bb97b46efa28ef8622abce662d6ab
Biff Barry <cpbarry80@gmail.com>
2021-12-11 14:27:31 -0500

    3a and 3c work

--- filters.py
+++ filters.py
@@ -17,6 +17,7 @@ iterator.
 You'll edit this file in Tasks 3a and 3c.
 """
 import operator
+import itertools
 
 
 class UnsupportedCriterionError(NotImplementedError):
@@ -72,6 +73,56 @@ class AttributeFilter:
         return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, value={self.value})"
 
 
+class DateFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        """
+        param: approach a CloseApproach object.
+        return: [datetime.datetime]: Converted time to datetime object.
+        """
+        return approach.time.date()
+
+
+class DistanceFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        """
+        param: approach a CloseApproach object.
+        return: [float]: Returns the distance of a CloseApproach.
+        """
+        return approach.distance
+
+
+class VelocityFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        """
+        param: approach a CloseApproach object.
+        return: [float]: Returns the velocity of a CloseApproach.
+        """
+        return approach.velocity
+
+
+class DiameterFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        """
+        param: approach a CloseApproach object.
+        return: [float]: Returns the diameter of a NearEarthObject object.
+        """
+        return approach.neo.diameter
+
+
+class HazardousFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        """
+        param: approach a CloseApproach object.
+        return: [float]: Returns the hazardous attribute of a NearEarthObject object.
+        """
+        return approach.neo.hazardous
+
+
 def create_filters(
         date=None, start_date=None, end_date=None,
         distance_min=None, distance_max=None,
@@ -109,14 +160,27 @@ def create_filters(
     :return: A collection of filters for use with `query`.
     """
 
-    collection_of_attribute_filters = []
-    for varname in create_filters.__code__.co_varnames:
-        if varname == hazardous:
-            collection_of_attribute_filters.append(AttributeFilter(op, varname))
-        if varname[-3:] == "min" and varname is not None:
-            collection_of_attribute_filters.append(AttributeFilter(operator.ge, varname))
-        if varname[-3:] == "max" and varname is not None:
-            collection_of_attribute_filters.append(AttributeFilter(operator.le, varname))
+    collection_of_attribute_filters = {}
+    if date:
+        collection_of_attribute_filters["date"] = DateFilter(operator.eq, date)
+    if start_date:
+        collection_of_attribute_filters["start_date"] = DateFilter(operator.ge, start_date)
+    if end_date:
+        collection_of_attribute_filters["end_date"] = DateFilter(operator.le, end_date)
+    if distance_min:
+        collection_of_attribute_filters["distance_min"] = DistanceFilter(operator.ge, distance_min)
+    if distance_max:
+        collection_of_attribute_filters["distance_max"] = DistanceFilter(operator.le, distance_max)
+    if velocity_min:
+        collection_of_attribute_filters["velocity_min"] = VelocityFilter(operator.ge, velocity_min)
+    if velocity_max:
+        collection_of_attribute_filters["velocity_max"] = VelocityFilter(operator.le, velocity_max)
+    if diameter_min:
+        collection_of_attribute_filters["diameter_min"] = DiameterFilter(operator.ge, diameter_min)
+    if diameter_max:
+        collection_of_attribute_filters["diameter_max"] = DiameterFilter(operator.le, diameter_max)
+    if hazardous is not None:
+        collection_of_attribute_filters["hazardous"] = HazardousFilter(operator.eq, hazardous)
     
     return collection_of_attribute_filters
 
@@ -131,5 +195,6 @@ def limit(iterator, n=None):
     :param n: The maximum number of values to produce.
     :yield: The first (at most) `n` values from the iterator.
     """
-    # TODO: Produce at most `n` values from the given iterator.
-    return iterator
+    if n == 0 or n is None:
+        return iterator
+    return list(itertools.islice(iterator, 0, n))
