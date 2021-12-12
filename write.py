95768fcec9cab5dc72be8413f428e04654260744
Biff Barry <cpbarry80@gmail.com>
2021-12-12 18:37:52 -0500

    4

--- write.py
+++ write.py
@@ -16,11 +16,9 @@ import json
 
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
-
     The precise output specification is in `README.md`. Roughly, each output row
     corresponds to the information in a single close approach from the `results`
     stream and its associated near-Earth object.
-
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
@@ -28,18 +26,40 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+
+    with open(filename, "w", newline="") as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for result in results:
+            info = {**result.serialize(), **result.neo.serialize()}
+            info["name"] = info["name"] if info["name"] is not None else ""
+            info["potentially_hazardous"] = "True" if info["potentially_hazardous"] else "False"
+            writer.writerow(info)
 
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
-
     The precise output specification is in `README.md`. Roughly, the output is a
     list containing dictionaries, each mapping `CloseApproach` attributes to
     their values and the 'neo' key mapping to a dictionary of the associated
     NEO's attributes.
-
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    data = []
+    for result in results:
+        info = {**result.serialize(), **result.neo.serialize()}
+        info["name"] = info["name"] if info["name"] is not None else ""
+        info["potentially_hazardous"] = bool(1) if info["potentially_hazardous"] else bool(0)
+        data.append(
+            {"datetime_utc": info["datetime_utc"],
+                "distance_au": info["distance_au"],
+                "velocity_km_s": info["velocity_km_s"],
+                "neo": {
+                    "designation": info["designation"],
+                    "name": info["name"],
+                    "diameter_km": info["diameter_km"],
+                    "potentially_hazardous": info["potentially_hazardous"],},})
+
+    with open(filename, "w") as outfile:
+        json.dump(data, outfile, indent="\t")
